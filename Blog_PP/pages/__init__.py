0a1deb7d8d3bcd8fa5025ad9eb460ede395b445e
LizbethFV <117942020+LizbethFV@users.noreply.github.com>
2025-05-27 19:45:19 -0600

    Delete Blog_PP directory

--- Blog_PP/pages/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 21 19:03:10 2025
-
-@author: periq
-"""
-
--- Blog_PP/pages/pagina_Convenio.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 21 19:04:34 2025
-
-@author: periq
-"""
-
